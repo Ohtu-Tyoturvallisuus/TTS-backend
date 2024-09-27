22304e2b451890358c9dd73e84c855ac68a7a3ca
latoma <127959432+latoma@users.noreply.github.com>
2024-09-27 10:51:17 +0300

    Added default dict as string

--- api/models.py
+++ api/models.py
@@ -14,7 +14,7 @@ class Survey(models.Model):
   title = models.CharField(max_length=100)
   description = models.TextField(blank=True)
   created_at = models.DateTimeField(auto_now_add=True)
-  risks = models.JSONField(default=dict)
+  risks = models.JSONField(default="{}")
 
   def __str__(self):
     return self.title
