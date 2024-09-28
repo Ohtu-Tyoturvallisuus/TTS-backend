c49796d6398a7e8ad1128ccce53148367a727778
latoma <127959432+latoma@users.noreply.github.com>
2024-09-28 20:30:18 +0300

    Added STATIC_URL

--- tts/production.py
+++ tts/production.py
@@ -43,6 +43,7 @@ MIDDLEWARE = COMMON_MIDDLEWARE + [
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
 conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
