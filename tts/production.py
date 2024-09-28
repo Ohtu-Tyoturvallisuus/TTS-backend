0ea3d8042308a1de62d3ecee068a6cd3426fb1ce
latoma <127959432+latoma@users.noreply.github.com>
2024-09-28 17:34:20 +0300

    Turned on debug for azure debugging

--- tts/production.py
+++ tts/production.py
@@ -5,6 +5,7 @@ import os
 from .middleware import COMMON_MIDDLEWARE
 from .settings import BASE_DIR
 
+DEBUG = True
 # Configure the domain name using the environment variable
 # that Azure automatically creates for us.
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
