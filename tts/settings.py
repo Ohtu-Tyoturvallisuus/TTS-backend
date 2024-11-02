3d5a35a704a7af29e3c513bae3445bde3209db35
Thomas Grundström <thomas.grundstrom@helsinki.fi>
2024-11-02 13:08:24 +0200

    Activate access token middleware

--- tts/settings.py
+++ tts/settings.py
@@ -64,7 +64,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-#    'api.middleware.access_token_middleware.AccessTokenMiddleware'
+    'api.middleware.access_token_middleware.AccessTokenMiddleware'
 ]
 ROOT_URLCONF = 'tts.urls'
 
