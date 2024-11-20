4d4aad085a950a6031d09f58b1f77ec4f2a4055f
Thomas Grundström <thomas.grundstrom@helsinki.fi>
2024-11-20 10:47:05 +0200

    Add database migration commands to startup file

--- startup.sh
+++ startup.sh
@@ -3,5 +3,9 @@
 # Install ffmpeg
 apt-get update && apt-get install -y ffmpeg
 
+# Apply database migrations
+python manage.py makemigrations || exit 1
+python manage.py migrate || exit 1
+
 # Run app
 gunicorn --bind=0.0.0.0 --timeout 600 tts.wsgi
