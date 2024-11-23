c0f8c20be0200234f24df13df62a9e0e1e5310bb
Thomas Grundström <thomas.grundstrom@helsinki.fi>
2024-11-23 16:18:34 +0200

    Remove database migration commands from startup file

--- startup.sh
+++ startup.sh
@@ -3,9 +3,5 @@
 # Install ffmpeg
 apt-get update && apt-get install -y ffmpeg
 
-# Apply database migrations
-python manage.py makemigrations || exit 1
-python manage.py migrate || exit 1
-
 # Run app
 gunicorn --bind=0.0.0.0 --timeout 600 tts.wsgi
