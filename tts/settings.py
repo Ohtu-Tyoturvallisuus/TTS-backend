a36730abdb8738a88ee7d2a3d03e660bc1a6e9c1
Vesa Rautiainen <veskurau@gmail.com>
2024-11-29 12:30:27 +0200

    Add functionality to settings.py fetch environment variables from key vault instead of app service

--- tts/settings.py
+++ tts/settings.py
@@ -39,11 +39,12 @@ def get_secret(secret_name):
     return client.get_secret(secret_name).value
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', '')
+SECRET_KEY = get_secret('SECRET-KEY')
 
 # Get speech key and service region from .env
-SPEECH_KEY = os.getenv('SPEECH_KEY')
-SPEECH_SERVICE_REGION = os.getenv('SPEECH_SERVICE_REGION')
+SPEECH_KEY = get_secret('SPEECH-KEY')
+SPEECH_SERVICE_REGION = get_secret('SPEECH-SERVICE-REGION')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -174,22 +175,22 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 # Azure Storage settings
-AZURE_STORAGE_ACCOUNT_NAME = os.getenv('AZURE_STORAGE_ACCOUNT_NAME')
-AZURE_STORAGE_ACCOUNT_KEY = os.getenv('AZURE_STORAGE_ACCOUNT_KEY')
-AZURE_CONTAINER_NAME = os.getenv('AZURE_CONTAINER_NAME')
+AZURE_STORAGE_ACCOUNT_NAME = get_secret('AZURE-STORAGE-ACCOUNT-NAME')
+AZURE_STORAGE_ACCOUNT_KEY = get_secret('AZURE-STORAGE-ACCOUNT-KEY')
+AZURE_CONTAINER_NAME = get_secret('AZURE-CONTAINER-NAME')
 
 # Azure text translator settings
-TRANSLATOR_KEY = os.getenv('TRANSLATOR_KEY')
-TRANSLATOR_SERVICE_REGION = os.getenv('TRANSLATOR_SERVICE_REGION')
-TRANSLATOR_ENDPOINT = os.getenv('TRANSLATOR_ENDPOINT')
+TRANSLATOR_KEY = get_secret('TRANSLATOR-KEY')
+TRANSLATOR_SERVICE_REGION = get_secret('TRANSLATOR-SERVICE-REGION')
+TRANSLATOR_ENDPOINT = get_secret('TRANSLATOR-ENDPOINT')
 
 # Entra ID settings
-CLIENT_ID = os.getenv('CLIENT_ID')
-TENANT_ID = os.getenv('TENANT_ID')
+CLIENT_ID = get_secret('CLIENT-ID')
+TENANT_ID = get_secret('TENANT-ID')
 
 # ERP settings
-ERP_CLIENT_ID = os.getenv('ERP_CLIENT_ID')
-ERP_CLIENT_SECRET = os.getenv('ERP_CLIENT_SECRET')
-ERP_TENANT_ID = os.getenv('ERP_TENANT_ID')
-ERP_RESOURCE = os.getenv('ERP_RESOURCE')
-ERP_SANDBOX_RESOURCE = os.getenv('ERP_SANDBOX_RESOURCE')
+ERP_CLIENT_ID = get_secret('ERP-CLIENT-ID')
+ERP_CLIENT_SECRET = get_secret('ERP-CLIENT-SECRET')
+ERP_TENANT_ID = get_secret('ERP-TENANT-ID')
+ERP_RESOURCE = get_secret('ERP-RESOURCE')
+ERP_SANDBOX_RESOURCE = get_secret('ERP-SANDBOX-RESOURCE')
